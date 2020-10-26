422935a22d6316e88ed4bb9ce85d30b9672efec2
magiqapps <info@magiqapps.com>
2020-10-25 21:29:49 -0500

    admin settings

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -5,10 +5,15 @@ const Admin = mongoose.model("Admin");
 
 const router = express.Router();
 
-router.get("/admin/rewardCredits", async (req, res) => {
-  const rewardCredits = await Admin.find();
-  console.log(rewardCredits);
+router.get("/admin/settings", async (req, res) => {
+  const rewardCredits = await Admin.findOne().rewardCredits;
   res.send(rewardCredits);
+});
+
+router.post("/admin/settings", async (req, res) => {
+  const settings = new Admin(req.body);
+  await settings.save();
+  res.send(settings);
 });
 
 module.exports = router;
