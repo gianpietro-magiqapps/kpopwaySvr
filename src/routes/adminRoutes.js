424667f6c2e05f4ac26df47fd0dea717d4959982
magiqapps <info@magiqapps.com>
2020-10-25 21:23:04 -0500

    admin settings

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -8,6 +8,7 @@ const router = express.Router();
 router.get("/admin/rewardCredits", async (req, res) => {
   const rewardCredits = await Admin.find({ id: "5f9631bd6bb65c33c141c2bb" })
     .rewardCredits;
+  console.log(rewardCredits);
   res.send(rewardCredits);
 });
 
