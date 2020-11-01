329945b29f24540f73f11a7d011cf554a76feba6
magiqapps <info@magiqapps.com>
2020-11-01 15:56:54 -0500

    add settings

--- src/models/Setting.js
+++ src/models/Setting.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 
 const settingSchema = new mongoose.Schema({
   rewardCredits: String,
+  calendarImage: String,
+  rankingTitle: String,
+  rankingDeadline: String,
+  rankingRestart: String,
 });
 
 mongoose.model("Setting", settingSchema);
